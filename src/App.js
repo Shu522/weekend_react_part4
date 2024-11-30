214cc050a12e417babaa581fef9efc7fb043025d
Shu522 <aki4358@naver.com>
2024-11-30 13:52:56 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import Icon from "./presentation/icon/Icon";
+// import { FaHome } from "react-icons/fa";
+// import { FaSearch } from "react-icons/fa";
+// import Button from "./common/Button.jsx";
+// import FruitButton from "./common/FruitButton.jsx";
+import Count from "./common/Count.jsx";
+import Input from "./common/Input.jsx";
+import Register from "./common/register.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Icon icon={<FaHome />} title={"Home"} />
+      <Icon icon={<FaSearch />} title={"Search"} />
+      <Button coffee={"아메리카노"} num={3} />
+      <Button coffee={"초코라떼"} num={99} /> */}
+      {/* <FruitButton a={"aa"} b={"b"} /> */}
+      {/* <Count a={0} />
+      <Input /> */}
+      <Register />
+    </div>
+  );
+}
+
+export default App;
